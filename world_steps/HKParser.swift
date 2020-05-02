d90d26bb36c78710383e8014b7b8d59d76db24ea
maxi <max.lengdell@gmail.com>
2020-05-02 22:27:29 +0200

    Chaos but will sort it out

---
+++ world_steps/HKParser.swift
@@ -0,0 +1,174 @@
+//
+//  HKParser.swift
+//  world_steps
+//
+//  Created by Max lengdell on 2020-05-02.
+//  Copyright © 2020 Max lengdell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import HealthKit
+
+func getStepsMonth(completion: @escaping (Double) -> Void)
+{
+    let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    let now = Date()
+    var interval = DateComponents()
+    interval.day = 30
+    var dateComponents = DateComponents()
+    dateComponents.setValue(-30, for: .day);
+    let startOfCount = Calendar.current.date(byAdding: dateComponents, to: now)
+    let startOfStart = Calendar.current.startOfDay(for: startOfCount!)
+    print("date to start: \(startOfStart)")
+
+    let query = HKStatisticsCollectionQuery(quantityType: type,
+                                            quantitySamplePredicate: nil,
+                                            options: [.cumulativeSum],
+                                            anchorDate: startOfStart,
+                                            intervalComponents: interval)
+    query.initialResultsHandler = { _, result, error in
+        var resultCount = 0.0
+        result?.enumerateStatistics(from: startOfStart, to: now) { statistics, _ in
+            
+            if let sum = statistics.sumQuantity() {
+                // Get steps (they are of double type)
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            } // end if
+            
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+    }
+    query.statisticsUpdateHandler = {
+        query, statistics, statisticsCollection, error in
+        
+        // If new statistics are available
+        if let sum = statistics?.sumQuantity() {
+            let resultCount = sum.doubleValue(for: HKUnit.count())
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        } // end if
+    }
+    healthStore.execute(query)
+    print("executed month")
+}
+func getStepsWeek(completion: @escaping (Double) -> Void)
+{
+    let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    let now = Date()
+    var interval = DateComponents()
+    interval.day = 7
+    var dateComponents = DateComponents()
+    dateComponents.setValue(-7, for: .day);
+    let startOfCount = Calendar.current.date(byAdding: dateComponents, to: now)
+    let startOfStart = Calendar.current.startOfDay(for: startOfCount!)
+    print("date to start: \(startOfStart)")
+    print("now \(now)")
+
+    let query = HKStatisticsCollectionQuery(quantityType: type,
+                                            quantitySamplePredicate: nil,
+                                            options: [.cumulativeSum],
+                                            anchorDate: startOfStart,
+                                            intervalComponents: interval)
+    query.initialResultsHandler = { _, result, error in
+        var resultCount = 0.0
+        result?.enumerateStatistics(from: startOfStart, to: now) { statistics, _ in
+            
+            if let sum = statistics.sumQuantity() {
+                // Get steps (they are of double type)
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            } // end if
+            
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+    }
+    query.statisticsUpdateHandler = {
+        query, statistics, statisticsCollection, error in
+        
+        // If new statistics are available
+        if let sum = statistics?.sumQuantity() {
+            let resultCount = sum.doubleValue(for: HKUnit.count())
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        } // end if
+    }
+    healthStore.execute(query)
+    print("executed week")
+}
+func getStepsDay(completion: @escaping (Double) -> Void)
+{
+    let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    let now = Date()
+    var interval = DateComponents()
+    interval.day = 1
+    let startOfStart = Calendar.current.startOfDay(for: now)
+//    var dateComponents = DateComponents()
+//    dateComponents.setValue(-1, for: .day);
+//    let startOfCount = Calendar.current.date(byAdding: dateComponents, to: now)
+//    let startOfStart = Calendar.current.startOfDay(for: startOfCount!)
+//    print("date to start: \(startOfStart)")
+    
+    let query = HKStatisticsCollectionQuery(quantityType: type,
+                                            quantitySamplePredicate: nil,
+                                            options: [.cumulativeSum],
+                                            anchorDate: startOfStart,
+                                            intervalComponents: interval)
+    query.initialResultsHandler = { _, result, error in
+        var resultCount = 0.0
+        result?.enumerateStatistics(from: startOfStart, to: now) { statistics, _ in
+            
+            if let sum = statistics.sumQuantity() {
+                // Get steps (they are of double type)
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            } // end if
+            
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+    }
+    query.statisticsUpdateHandler = {
+        query, statistics, statisticsCollection, error in
+        
+        // If new statistics are available
+        if let sum = statistics?.sumQuantity() {
+            let resultCount = sum.doubleValue(for: HKUnit.count())
+            // Return
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        } // end if
+    }
+    healthStore.execute(query)
+    print("executed day")
+}
+func authorize(){
+    let writableTypes: Set<HKSampleType> =
+        [HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
+    let readableTypes: Set<HKSampleType> = [HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
+    
+    guard HKHealthStore.isHealthDataAvailable() else {
+        print("false on availability");
+        return
+    }
+    //let healthKitTypes: Set = [ HKObjectType.quantityType(forIdentifier:HKQuantityTypeIdentifier.stepCount)! ]
+    //Check for authorization
+    healthStore.requestAuthorization(toShare: writableTypes, read: readableTypes) { (success, error) in
+        if (success) {
+            //Authorization successful
+            print("Authenticated");
+        }
+    }
+    // Do any additional setup after loading the view.
+}
--- world_steps/ViewController.swift
+++ world_steps/ViewController.swift
@@ -11,120 +11,52 @@ import HealthKit
 let healthStore = HKHealthStore()
 class ViewController: UIViewController {
     @IBOutlet weak var stepsLabel: UILabel!
+    @IBOutlet weak var stepsLabelWeek: UILabel!
+    @IBOutlet weak var stepsLabelDay: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Access Step Count
         authorize()
         // Authorization Successful
-        self.getSteps { (result) in
+        
+        getStepsMonth { (result) in
             DispatchQueue.main.async {
                 let stepCount = String(Int(result))
                 self.stepsLabel.text = String(stepCount)
+                print("steps: \(stepCount)");
+                
             }
         }
-        
+        getStepsWeek { (result) in
+            DispatchQueue.main.async {
+                let stepCount1 = String(Int(result))
+                self.stepsLabelWeek.text = String(stepCount1)
+                print("steps week: \(stepCount1)");
+                
+            }
+        }
+        getStepsDay { (result) in
+            DispatchQueue.main.async {
+                let stepCount = String(Int(result))
+                self.stepsLabelDay.text = String(stepCount)
+                print("steps a Day: \(stepCount)");
+                
+            }
+        }
     }
-    func authorize(){
-        let writableTypes: Set<HKSampleType> =
-                  [HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
-              let readableTypes: Set<HKSampleType> = [HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!]
-
-              guard HKHealthStore.isHealthDataAvailable() else {
-                  print("false on availability");
-                  return
-              }
-              //let healthKitTypes: Set = [ HKObjectType.quantityType(forIdentifier:HKQuantityTypeIdentifier.stepCount)! ]
-              //Check for authorization
-              healthStore.requestAuthorization(toShare: writableTypes, read: readableTypes) { (success, error) in
-                  if (success) {
-                      //Authorization successful
-                      print("Authenticated");
-                  }
-              }
-              // Do any additional setup after loading the view.
-          }
     
-    func getSteps(completion: @escaping (Double) -> Void)
-    {
-  let type = HKQuantityType.quantityType(forIdentifier: .stepCount)!
-      
-  let now = Date()
-  let startOfDay = Calendar.current.startOfDay(for: now)
-  var interval = DateComponents()
-  interval.day = 1
-        let query = HKStatisticsCollectionQuery(quantityType: type,
-  quantitySamplePredicate: nil,
-  options: [.cumulativeSum],
-  anchorDate: startOfDay,
-  intervalComponents: interval)
-        query.initialResultsHandler = { _, result, error in
-                var resultCount = 0.0
-                result!.enumerateStatistics(from: startOfDay, to: now) { statistics, _ in
-
-                if let sum = statistics.sumQuantity() {
-                    // Get steps (they are of double type)
-                    resultCount = sum.doubleValue(for: HKUnit.count())
-                } // end if
-
-                // Return
-                DispatchQueue.main.async {
-                    completion(resultCount)
-                }
-            }
-        }
-        query.statisticsUpdateHandler = {
-            query, statistics, statisticsCollection, error in
-
-            // If new statistics are available
-            if let sum = statistics?.sumQuantity() {
-                let resultCount = sum.doubleValue(for: HKUnit.count())
-                // Return
-                DispatchQueue.main.async {
-                    completion(resultCount)
-                }
-            } // end if
-        }
-        healthStore.execute(query)
-}
-    /*
-    func readSteps(completion: (_ stepsRetrieved: Double) -> Void) {
-        //Define quantity of data
-        let stepsCount = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
-        let date = Date()
-        let cal = Calendar(identifier: Calendar.Identifier.gregorian)
-        let newDate = cal.startOfDay(for: date)
-        let startOfDay = Calendar.current.startOfDay(for: Date())
-
-        //Set predicates & Interval
-        let predicate = HKQuery.predicateForSamples(withStart: newDate, end: date, options: .strictStartDate)
-        var interval = DateComponents()
-        interval.day = 1
+    @IBAction func update(_ sender: Any) {
+        print("test")
         
-        //Perform query
-        let query = HKStatisticsCollectionQuery(quantityType: stepsCount!, quantitySamplePredicate: predicate, options: [.cumulativeSum], anchorDate: newDate as Date, intervalComponents: interval)
-        
-        query.initialResultsHandler = { query, results, error in
-            if error != nil {
-                //Something went wrong
-                return
-            }
-            
-                if let myResults = results{
-                myResults.enumerateStatistics(from: startOfDay, to: date) {
-                    statistics, stop in
-                    
-                    if let quantity = statistics.sumQuantity() {
-
-                                           let steps = quantity.doubleValue(for: HKUnit.count())
-
-                                           print("Steps = \(steps)")
-                                           completion(stepRetrieved: steps)
-                    }
-                }
+        getStepsDay { (result) in
+            DispatchQueue.main.async {
+                let stepCount = String(Int(result))
+                self.stepsLabelDay.text = String(stepCount)
+                print("stepsDay: \(stepCount)");
+                
             }
         }
-        healthStore.execute(query)
-    }*/
+    }
 }
 
